bb5c3b56c72a3b6e3682affc87c26cfe9a50cd66
Brandon Mills <sllimnodnarb@gmail.com>
2018-09-06 12:19:34 -0500

    moving notify_wellfence in controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -53,11 +53,7 @@ class PeopleController < ApplicationController
     end
   end
 
-  private
 
-  def person_params
-    params.require(:person).permit(:first_name, :last_name, :company, :position, :email, :phone, :h2s, :has_card, :card_number, :rig, :id)
-  end
 
   def notify_wellfence
     account_sid = ENV['ACCOUNT_SID']
@@ -69,6 +65,12 @@ class PeopleController < ApplicationController
       from: "+18306421354",
         to: ["+12104008165", "+12544854214"],
       body: "#{@person.first_name} #{@person.last_name} \n #{@person.company} \n #{@person.position} \n #{@person.email} \n Phone: #{@person.phone} \n H2S: #{@person.h2s} \n ID:#{@person.card_number}")
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :company, :position, :email, :phone, :h2s, :has_card, :card_number, :rig, :id)
   end
 
 end
